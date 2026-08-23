e2f4822fdba5bd587403fc70dde17d8821981148
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:04:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,12 +8,11 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake      \
-    libdecor   \
     sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building RigelEngine..."
 echo "---------------------------------------------------------------"
